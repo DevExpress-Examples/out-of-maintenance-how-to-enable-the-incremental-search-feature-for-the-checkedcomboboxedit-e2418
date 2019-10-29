4d48c49e9a5e90bc26223953a6bd126175ec0b82
DevExpressExampleBot <fakeEmail>
2019-10-29 21:17:18 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication31/Form1.Designer.vb
+++ VB/WindowsApplication31/Form1.Designer.vb
@@ -1,6 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace DXSample
+﻿Namespace DXSample
 	Partial Public Class Form1
 		''' <summary>
 		''' Required designer variable.
@@ -26,66 +24,41 @@ Namespace DXSample
 		''' </summary>
 		Private Sub InitializeComponent()
 			Me.components = New System.ComponentModel.Container()
-			Me.employeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.nwindDataSet = New WindowsApplication31.nwindDataSet()
-			Me.employeesTableAdapter = New WindowsApplication31.nwindDataSetTableAdapters.EmployeesTableAdapter()
-			Me.customEdit1 = New DXSample.CustomEdit()
-			CType(Me.employeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.customEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.checkedComboBoxEdit = New DevExpress.XtraEditors.CheckedComboBoxEdit()
+			CType(Me.checkedComboBoxEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
 			Me.SuspendLayout()
 			' 
-			' employeesBindingSource
+			' checkedComboBoxEdit
 			' 
-			Me.employeesBindingSource.DataMember = "Employees"
-			Me.employeesBindingSource.DataSource = Me.nwindDataSet
-			' 
-			' nwindDataSet
-			' 
-			Me.nwindDataSet.DataSetName = "nwindDataSet"
-			Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' employeesTableAdapter
-			' 
-			Me.employeesTableAdapter.ClearBeforeFill = True
-			' 
-			' customEdit1
-			' 
-			Me.customEdit1.EditValue = ""
-			Me.customEdit1.Location = New System.Drawing.Point(44, 83)
-			Me.customEdit1.Name = "customEdit1"
-			Me.customEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-			Me.customEdit1.Properties.DataSource = Me.employeesBindingSource
-			Me.customEdit1.Properties.DisplayMember = "LastName"
-			Me.customEdit1.Properties.IncrementalSearch = True
-			Me.customEdit1.Properties.ValueMember = "FirstName"
-			Me.customEdit1.Size = New System.Drawing.Size(415, 22)
-			Me.customEdit1.TabIndex = 0
+			Me.checkedComboBoxEdit.EditValue = ""
+			Me.checkedComboBoxEdit.Location = New System.Drawing.Point(33, 67)
+			Me.checkedComboBoxEdit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+			Me.checkedComboBoxEdit.Name = "checkedComboBoxEdit"
+			Me.checkedComboBoxEdit.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+			Me.checkedComboBoxEdit.Properties.DisplayMember = "LastName"
+			Me.checkedComboBoxEdit.Properties.IncrementalSearch = True
+			Me.checkedComboBoxEdit.Properties.ValueMember = "FirstName"
+			Me.checkedComboBoxEdit.Size = New System.Drawing.Size(311, 20)
+			Me.checkedComboBoxEdit.TabIndex = 0
 			' 
 			' Form1
 			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
 			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(519, 322)
-			Me.Controls.Add(Me.customEdit1)
-			Me.Margin = New System.Windows.Forms.Padding(4)
+			Me.ClientSize = New System.Drawing.Size(389, 262)
+			Me.Controls.Add(Me.checkedComboBoxEdit)
 			Me.Name = "Form1"
 			Me.Text = "Incremental Search"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.employeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.customEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
+			CType(Me.checkedComboBoxEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
 			Me.ResumeLayout(False)
 
 		End Sub
 
 		#End Region
 
-		Private customEdit1 As CustomEdit
-		Private nwindDataSet As WindowsApplication31.nwindDataSet
-		Private employeesBindingSource As System.Windows.Forms.BindingSource
-		Private employeesTableAdapter As WindowsApplication31.nwindDataSetTableAdapters.EmployeesTableAdapter
-
+		Private checkedComboBoxEdit As DevExpress.XtraEditors.CheckedComboBoxEdit
 	End Class
 End Namespace
 
--- VB/WindowsApplication31/Form1.vb
+++ VB/WindowsApplication31/Form1.vb
@@ -1,25 +1,43 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
+﻿Imports System
 Imports System.ComponentModel
-Imports System.Data
-Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
-Imports System.Collections
-
 
 Namespace DXSample
 	Partial Public Class Form1
 		Inherits Form
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
-
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' TODO: This line of code loads data into the 'nwindDataSet.Employees' table. You can move, or remove it, as needed.
-			Me.employeesTableAdapter.Fill(Me.nwindDataSet.Employees)
-
+		Private Function GetCustomers() As BindingList(Of Customer)
+			Dim customers As New BindingList(Of Customer)()
+			customers.Add(New Customer() With {
+				.Id = 0,
+				.Name = "Alex",
+				.IsActive = True
+			})
+			customers.Add(New Customer() With {
+				.Id = 1,
+				.Name = "Sarah",
+				.IsActive = True
+			})
+			customers.Add(New Customer() With {
+				.Id = 2,
+				.Name = "Bob",
+				.IsActive = False
+			})
+			Return customers
+		End Function
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			checkedComboBoxEdit.Properties.IncrementalSearch = True
+			checkedComboBoxEdit.Properties.DataSource = GetCustomers()
+			checkedComboBoxEdit.Properties.DisplayMember = "Name"
+			checkedComboBoxEdit.Properties.ValueMember = "Id"
 		End Sub
+	End Class
+	Friend Class Customer
+		Public Property Id() As Integer
+		Public Property Name() As String
+		Public Property IsActive() As Boolean
 	End Class
 End Namespace
